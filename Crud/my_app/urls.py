8dc775ddee1170624f13a406c5e70de35fb23449
2DYXBOSS <pirateavie225@gmail.com>
2024-01-20 02:23:00 +0000

    delete , update ok

--- Crud/my_app/urls.py
+++ Crud/my_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import create_view,list_view ,detail_view
+from .views import create_view,list_view ,detail_view,update,delete
 urlpatterns = [
     path('home/',create_view, name="home"),
+    path('delete/<int:id>/',delete, name="delete"),
+    path('update/<int:id>/',update, name="update"),
     path('list/',list_view, name="list-views"),
     path('<int:id>',detail_view, name='detail_view'),
 ]
--- Crud/my_app/views.py
+++ Crud/my_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here.
 from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import get_object_or_404
 
 from .forms import ArticleForm
 from .models import Article
@@ -28,3 +28,34 @@ def detail_view(request, id):
     context={}
     context["data"]=Article.objects.get(id=id)
     return render(request, "detail_view.html", context)
+
+
+# def update(request, id):
+#     x = Article.objects.get(id=id)
+#     x.description = "description"
+#     x.titre = "titre"
+
+#     x.save()
+#     return render(request,"update.html",context= {"context":x})
+
+
+def update(request, id):
+    
+    x = Article.objects.get(id=id)
+    
+    context={} 
+    form = ArticleForm(request.POST or None)
+    x.description  = str(form["description"].value())  
+    x.titre = str(form["titre"].value()) 
+    x.save()
+    # print(form["titre"].value())
+    # print(form["description"].value())
+    context['form']=form
+    return render(request,"update.html", context)
+
+def delete(request, id):
+    context={}
+    context["datas"]=Article.objects.all()
+    x = Article.objects.get(id=id)
+    x.delete()
+    return render(request,"list.html", context)
